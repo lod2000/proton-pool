c65e2b6d0f1f8c9ad8d94475be89c0ccdafa76a9
lod2000 <lod.wolflovers@gmail.com>
2017-01-06 10:41:09 -0800

    first commit

---
+++ android/src/com/spacenerd/protonpool/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.spacenerd.protonpool;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.spacenerd.protonpool.ProtonPool;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ProtonPool(), config);
+	}
+}
---
+++ desktop/src/com/spacenerd/protonpool/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.spacenerd.protonpool.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.spacenerd.protonpool.ProtonPool;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ProtonPool(), config);
+	}
+}
