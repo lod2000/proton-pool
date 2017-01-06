72d9dbd1d10668cf964347a39893f6717482dca4
lod2000 <lod.wolflovers@gmail.com>
2017-01-06 13:34:56 -0800

    added user proton placement

--- core/src/com/spacenerd/protonpool/ProtonPool.java
+++ core/src/com/spacenerd/protonpool/ProtonPool.java
@@ -2,6 +2,7 @@ package com.spacenerd.protonpool;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
@@ -10,18 +11,22 @@ import com.badlogic.gdx.math.Vector2;
 
 import java.util.ArrayList;
 
-public class ProtonPool extends ApplicationAdapter {
+public class ProtonPool extends ApplicationAdapter implements InputProcessor {
 	private SpriteBatch batch;
 	private ArrayList<Proton> protons;
+    private int protonRadius = 30;
 	
 	@Override
 	public void create () {
+        Gdx.input.setInputProcessor(this);
+
         Proton.texture = new Texture(Gdx.files.internal("proton.png"));
 
 		batch = new SpriteBatch();
 
         protons = new ArrayList<Proton>();
-        protons.add(new Proton("TEST", new Vector2(200, 200), new Vector2(179, 96)));
+        protons.add(new Proton("Thing1", new Vector2(200, 200), new Vector2(179, 96)));
+        protons.add(new Proton("Thing2", new Vector2(800, 20), new Vector2(-20, 18)));
 	}
 
 	@Override
@@ -41,4 +46,52 @@ public class ProtonPool extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 	}
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        protons.add(new Proton("Thing" + protons.size(),
+                new Vector2(
+                        screenX - protonRadius,
+                        Gdx.graphics.getHeight() - screenY - protonRadius
+                ),
+                new Vector2(0, 0)
+        ));
+//        Gdx.app.log("ProtonPool", "" + screenY);
+        return true;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
 }
