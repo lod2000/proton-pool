2ecca7bf5ac0587f46a8953af095b78696ac434e
lod2000 <lod.wolflovers@gmail.com>
2017-01-07 11:03:04 -0800

    implemented collision circles (no collision detection yet), acceleration, vector lines

--- core/src/com/spacenerd/protonpool/Proton.java
+++ core/src/com/spacenerd/protonpool/Proton.java
@@ -12,25 +12,34 @@ public class Proton {
     public String name;
     public Vector2 position;
     public Vector2 velocity;
+    public Vector2 acceleration;
     private Sprite sprite;
+    public Circle collisionCircle;
 
-    public Proton(String name, Vector2 position, Vector2 velocity){
+    public static final int radius = 30;
+
+    public Proton(String name, Vector2 position, Vector2 velocity, Vector2 acceleration){
         this.name = name;
         this.position = position;
         this.velocity = velocity;
+        this.acceleration = acceleration;
         sprite = new Sprite(texture);
+        collisionCircle = new Circle(position, radius);
     }
 
     public void draw(SpriteBatch batch){
-        sprite.setPosition(position.x, position.y);
+        collisionCircle.setPosition(position);
+        sprite.setPosition(position.x - radius, position.y - radius);
         sprite.draw(batch);
     }
 
     public void step(){
-        if(position.x + sprite.getWidth() >= Gdx.graphics.getWidth() || position.x <= 0){
+        Gdx.app.log("Proton", acceleration + "");
+        velocity.add(acceleration.cpy().scl(Gdx.graphics.getDeltaTime()));
+        if(position.x + radius >= Gdx.graphics.getWidth() || position.x - radius <= 0){
             velocity.x *= -1;
         }
-        if(position.y + sprite.getHeight() >= Gdx.graphics.getHeight() || position.y <= 0){
+        if(position.y + radius >= Gdx.graphics.getHeight() || position.y - radius <= 0){
             velocity.y *= -1;
         }
         position.add(velocity.cpy().scl(Gdx.graphics.getDeltaTime()));
--- core/src/com/spacenerd/protonpool/ProtonPool.java
+++ core/src/com/spacenerd/protonpool/ProtonPool.java
@@ -5,8 +5,10 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 
 import java.util.ArrayList;
@@ -14,7 +16,9 @@ import java.util.ArrayList;
 public class ProtonPool extends ApplicationAdapter implements InputProcessor {
 	private SpriteBatch batch;
 	private ArrayList<Proton> protons;
-    private int protonRadius = 30;
+    private static final float forceConstant = 100000;
+    private ShapeRenderer shapeRenderer; //debugging only
+    private OrthographicCamera camera;
 	
 	@Override
 	public void create () {
@@ -25,8 +29,19 @@ public class ProtonPool extends ApplicationAdapter implements InputProcessor {
 		batch = new SpriteBatch();
 
         protons = new ArrayList<Proton>();
-        protons.add(new Proton("Thing1", new Vector2(200, 200), new Vector2(179, 96)));
-        protons.add(new Proton("Thing2", new Vector2(800, 20), new Vector2(-20, 18)));
+        protons.add(new Proton("Thing1",
+                new Vector2(200, 200),
+                new Vector2(100, 10),
+                new Vector2(0,0)
+        ));
+        protons.add(new Proton("Thing2",
+                new Vector2(800, 50),
+                new Vector2(-20, -18),
+                new Vector2(0,0)
+        ));
+
+        shapeRenderer = new ShapeRenderer();
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 	}
 
 	@Override
@@ -34,18 +49,59 @@ public class ProtonPool extends ApplicationAdapter implements InputProcessor {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+//        shapeRenderer.setProjectionMatrix(camera);
+
 		batch.begin();
         for(Proton proton: protons){
+            sumForce();
             proton.step();
             proton.draw(batch);
         }
 		batch.end();
+
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+        for(Proton proton: protons){
+            shapeRenderer.setColor(0, 0, 1, 1);
+            shapeRenderer.line(proton.position, proton.position.cpy().add(proton.acceleration.scl(100)));
+        }
+        shapeRenderer.end();
+
+//        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+//        for(Proton proton: protons){
+//            shapeRenderer.setColor(0, 1, 0, 1);
+//            shapeRenderer.line(proton.position, proton.position.cpy().add(proton.velocity.scl(10)));
+//        }
+//        shapeRenderer.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
 	}
+
+    public void sumForce(){
+        for(Proton target: protons){
+            Vector2 acceleration = new Vector2();
+            for(Proton other: protons){
+                if(other != target){
+                    Vector2 distance = target.position.cpy().sub(other.position.cpy());
+                    Vector2 partialAcceleration = new Vector2(1, 1)
+                            .setLength((float) (forceConstant / Math.pow(distance.len(), 2)))
+                            .setAngle(distance.angle());
+//                            (float) (forceConstant / Math.pow(distance.len2(), 2)),
+//                            (float) (forceConstant / Math.pow(distance.len, 2))
+//                    );
+//                    Gdx.app.log("ProtonPool", "" + partialAcceleration);
+                    acceleration.add(partialAcceleration);
+//                    if(target.collisionCircle.overlaps(other.collisionCircle)){
+//                        Vector2 distance = target.position.cpy().sub(other.position.cpy());
+//                        distance.rotate(90);
+//                    }
+                }
+            }
+            target.acceleration = acceleration;
+        }
+    }
 
     @Override
     public boolean keyDown(int keycode) {
@@ -64,14 +120,23 @@ public class ProtonPool extends ApplicationAdapter implements InputProcessor {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        if(screenX < Proton.radius){
+            screenX = Proton.radius;
+        }
+        if(Gdx.graphics.getWidth() - screenX < Proton.radius){
+            screenX = Gdx.graphics.getWidth() - Proton.radius;
+        }
+        if(screenY < Proton.radius){
+            screenY = Proton.radius;
+        }
+        if(Gdx.graphics.getHeight() - screenY < Proton.radius){
+            screenY = Gdx.graphics.getHeight() - Proton.radius;
+        }
         protons.add(new Proton("Thing" + protons.size(),
-                new Vector2(
-                        screenX - protonRadius,
-                        Gdx.graphics.getHeight() - screenY - protonRadius
-                ),
+                new Vector2(screenX, Gdx.graphics.getHeight() - screenY),
+                new Vector2(0, 0),
                 new Vector2(0, 0)
         ));
-//        Gdx.app.log("ProtonPool", "" + screenY);
         return true;
     }
 
